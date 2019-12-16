0d23beb5ff25892d5f9ffa6404351bac360c5b87
Raimbault Brieuc <braimbau@student.42.fr>
2019-12-16 16:43:37 +0100

    add pyramid:

--- srcs/minirtx.c
+++ srcs/minirtx.c
@@ -6,7 +6,7 @@
 /*   By: braimbau <braimbau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/22 19:03:43 by braimbau          #+#    #+#             */
-/*   Updated: 2019/12/16 13:30:42 by braimbau         ###   ########.fr       */
+/*   Updated: 2019/12/16 16:34:58 by braimbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,7 +130,6 @@ int main(int argc, char **argv)
 
 	r = NULL;
 	rtx = parseke(argc, argv);
-			printf("|%d %3f %3f %3f|\n",rtx.shape->type,rtx.shape->vec.x, rtx.shape->vec.y, rtx.shape->vec.z);
 	mlx_ptr = mlx_init();
 	mlx_win = mlx_new_window(mlx_ptr, rtx.res.x, rtx.res.y, "miniRTX");
 	img = mlx_new_image(mlx_ptr, rtx.res.x, rtx.res.y);
@@ -148,7 +147,6 @@ int main(int argc, char **argv)
 			tan((float)rtx.cam->fov /2 /180 * M_PI);
 			rtx.cam->ray.z = -1;
 			rtx.cam->ray = normalize(rtx.cam->ray);
-			printf("|%d %3f %3f %3f|\n",rtx.shape->type,rtx.shape->vec.x, rtx.shape->vec.y, rtx.shape->vec.z);
 			mlx_put_pixel_img(rtx.coor.x, rtx.coor.y, &id, rtx.res.x, cal_col(*(rtx.cam), rtx));
 			rtx.coor.y++;
 		}
