3d7e227ee1517f9ab9dd28e89529369a39e1b2e2
selgrabl <selgrabl@student.42.fr>
2020-01-09 18:49:36 +0100

    le cylindre qui refonctionne

--- srcs/tools4dist.c
+++ srcs/tools4dist.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tools4dist.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: braimbau <braimbau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: selgrabl <selgrabl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 14:10:01 by selgrabl          #+#    #+#             */
-/*   Updated: 2020/01/08 17:33:16 by braimbau         ###   ########.fr       */
+/*   Updated: 2020/01/09 18:41:59 by selgrabl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,8 +69,9 @@ float		find_dist_cy(t_vec origin, t_vec ray, t_tg *shape, t_vec pos)
 	c = dot(pos, pos) - pow(dot(pos, shape->vec), 2) - pow(shape->dia / 2, 2);
 	if ((x = pow(b, 2) - 4 * a * c) <= 0.00001)
 		return (-1.0);
-	if ((c = (-b - sqrtf(x)) / (2 * a)) == 0 &&
-	(a = (-b + sqrtf(x)) / (2 * a)) == 0)
+	c = (-b - sqrtf(x)) / (2 * a);
+	a = (-b + sqrtf(x)) / (2 * a);
+	if (a == 0 && c == 0)
 		return (-1.0);
 	shape->p1.x = (c > a) ? -1: 1;
 	b = (c > a) ? c : a;
@@ -80,11 +81,41 @@ float		find_dist_cy(t_vec origin, t_vec ray, t_tg *shape, t_vec pos)
 	if ((fabs(a) > shape->hi / 2) || (fabs(x) > shape->hi / 2 && c < 0))
 	{
 		a = dot(ray, shape->vec) * b + dot(pos, shape->vec);
-		x = dot(min(shape->center, origin), shape->vec);
 		if ((fabs(a) > shape->hi / 2) || (fabs(x) > shape->hi / 2 && b < 0))
 			return (-1.0);
 	}
 	shape->normal = fois(normalize(min(plus(origin, fois(ray, a)), plus(shape->center,
 	fois(shape->vec, dot(min(origin, shape->center), shape->vec))))), shape->p1.x);
-	return (a);
+	return (fabsf(a));
+    /*float a;
+    float b;
+    float c;
+    float x;
+
+    pos = min(origin, shape->center);
+    a = dot(ray, ray) - dot(ray, shape->vec) * dot(ray, shape->vec);
+    b = 2 * (dot(ray, pos) - dot(ray, shape->vec) * dot(pos, shape->vec));
+    c = dot(pos, pos) - pow(dot(pos, shape->vec), 2) - pow(shape->dia / 2, 2);
+    if ((x = pow(b, 2) - 4 * a * c) <= 0.0001)
+        return(-1.0);
+    c = (-b - sqrtf(x)) / (2 * a);
+    a = (-b + sqrtf(x)) / (2 * a);
+    if( a == 0 && c == 0)
+        return(-1.0);
+	shape->p1.x = (c > a) ? 1: 1;
+    b = (c > a) ? c : a;
+	c = (c > a) ? a : c;
+    a = dot(ray, shape->vec) * c + dot(pos, shape->vec);
+    x = dot(min(shape->center, origin), shape->vec);
+    if (( fabs(a) > shape->hi / 2) || (fabs(x) > shape->hi / 2 && c < 0))
+    {
+        c = b;
+        a = dot(ray, shape->vec) * c + dot(pos, shape->vec);
+        x = dot(min(shape->center, origin), shape->vec);
+        if (( fabs(a) > shape->hi / 2) || (fabs(x) > shape->hi / 2 && c < 0))
+            return(-1.0);
+	shape->normal = fois(normalize(min(plus(origin, fois(ray, a)), plus(shape->center,
+	fois(shape->vec, dot(min(origin, shape->center), shape->vec))))), shape->p1.x);
+    }
+    return (fabsf(a));*/
 }
