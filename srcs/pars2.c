bdf20ee72bfe927c77697048b6945055f475a9a8
Brieuc RAIMBAULT <braimbau@e3r9p12.42.fr>
2020-01-13 19:09:58 +0100

    [pars2.c] change order of parsing

--- srcs/pars2.c
+++ srcs/pars2.c
@@ -6,7 +6,7 @@
 /*   By: braimbau <braimbau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/30 17:07:13 by selgrabl          #+#    #+#             */
-/*   Updated: 2020/01/13 19:01:11 by braimbau         ###   ########.fr       */
+/*   Updated: 2020/01/13 19:06:36 by braimbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,71 @@ char		*pars_r(char **buf, t_rtx *rtx)
 	if (rtx->res.x < rtx->res.y)
 		return("X/Y must be supperior to 1");
 	return(NULL);
+}
+
+char		*pars_c(char **buf, t_rtx *rtx)
+{
+	char *ret;
+	t_cam *cam;
+
+	cam = malloc(sizeof(t_cam));
+	cam->next = rtx->cam;
+	rtx->cam = cam;
+	cam->filter = 0;
+	if (!buf[1] || !buf[2] || !buf[3])
+		return("Missing argument(s) on declaraton of camera");
+	if (buf[4] != NULL && buf[5] != NULL)
+		return("Too many arguments on declaration of camera");
+	ret = read_pos(buf[1], &(rtx->cam->origin), "of camera");
+	ret = join(ret, read_vec(buf[2], &(rtx->cam->vec), "of camera"));
+	rtx->cam->fov = ft_atoi(buf[3]);
+	if (rtx->cam->fov == -42)
+		return("Invalid number for FOV of camera");
+	if (rtx->cam->fov > 180 || rtx->cam->fov < 0)
+		return ("Value out of range for FOV of camera");
+	if (buf[4])
+	{
+		if (buf[4][0] == '3' && buf[4][1] == 'D' && buf[4][2] == 0)
+		{
+			cam->origin.x -= 2;
+			cam->filter = 'l';
+			buf[4][0] = 'r';
+			buf[4][1] = 0;
+			pars_c(buf, rtx);
+		}
+		else if ((buf[4][0] == 'R' || buf[4][0] == 'G' || buf[4][0] == 'B' || buf[4][0] == 'C' ||
+		buf[4][0] == 'Y' || buf[4][0] == 'P' || buf[4][0] == 'W' || buf[4][0] == 'N' ||
+		buf[4][0] == 'S' || buf[4][0] == 'r') && buf[4][1] == 0)
+		{
+				cam->filter = buf[4][0];
+		}
+		else
+			return ("Invalid value for filter of camera");
+	}
+	return(ret);
+}
+
+char		*pars_l(char **buf, t_rtx *rtx)
+{
+	char *ret;
+	t_light *light;
+
+	light = malloc(sizeof(t_light));
+	light->next = rtx->light;
+	rtx->light = light;
+	if (!buf[1] || !buf[2] || !buf[3])
+		return("Missing argument(s) on declaraton of light");
+	if (buf[4] != NULL)
+		return("Too many arguments on declaration of light");
+	ret = read_pos(buf[1], &(rtx->light->pos), "of light");
+	rtx->light->ratio = ft_atof(buf[2]);
+	if (isnan(rtx->light->ratio))
+		return("Invalid number for ratio of light");
+	if (rtx->light->ratio > 1 || rtx->light->ratio < 0)
+		return ("Value out of range for ratio of light");
+	ret = join(ret, read_color(buf[3], &(rtx->light->color), "of light"));
+	rtx->light->color = lfois(rtx->light->color, rtx->light->ratio);
+	return(ret);
 }
 
 char 		*pars_sq(char **buf, t_rtx *rtx)
@@ -154,71 +219,6 @@ char		*pars_tr(char **buf, t_rtx *rtx)
 	shape->vec = normalize(cross(min(shape->p2, shape->p1),
 		min(shape->p3, shape->p1)));
 	shape->center = shape->p1;
-	return(ret);
-}
-
-char		*pars_c(char **buf, t_rtx *rtx)
-{
-	char *ret;
-	t_cam *cam;
-
-	cam = malloc(sizeof(t_cam));
-	cam->next = rtx->cam;
-	rtx->cam = cam;
-	cam->filter = 0;
-	if (!buf[1] || !buf[2] || !buf[3])
-		return("Missing argument(s) on declaraton of camera");
-	if (buf[4] != NULL && buf[5] != NULL)
-		return("Too many arguments on declaration of camera");
-	ret = read_pos(buf[1], &(rtx->cam->origin), "of camera");
-	ret = join(ret, read_vec(buf[2], &(rtx->cam->vec), "of camera"));
-	rtx->cam->fov = ft_atoi(buf[3]);
-	if (rtx->cam->fov == -42)
-		return("Invalid number for FOV of camera");
-	if (rtx->cam->fov > 180 || rtx->cam->fov < 0)
-		return ("Value out of range for FOV of camera");
-	if (buf[4])
-	{
-		if (buf[4][0] == '3' && buf[4][1] == 'D' && buf[4][2] == 0)
-		{
-			cam->origin.x -= 2;
-			cam->filter = 'l';
-			buf[4][0] = 'r';
-			buf[4][1] = 0;
-			pars_c(buf, rtx);
-		}
-		else if ((buf[4][0] == 'R' || buf[4][0] == 'G' || buf[4][0] == 'B' || buf[4][0] == 'C' ||
-		buf[4][0] == 'Y' || buf[4][0] == 'P' || buf[4][0] == 'W' || buf[4][0] == 'N' ||
-		buf[4][0] == 'S' || buf[4][0] == 'r') && buf[4][1] == 0)
-		{
-				cam->filter = buf[4][0];
-		}
-		else
-			return ("Invalid value for filter of camera");
-	}
-	return(ret);
-}
-
-char		*pars_l(char **buf, t_rtx *rtx)
-{
-	char *ret;
-	t_light *light;
-
-	light = malloc(sizeof(t_light));
-	light->next = rtx->light;
-	rtx->light = light;
-	if (!buf[1] || !buf[2] || !buf[3])
-		return("Missing argument(s) on declaraton of light");
-	if (buf[4] != NULL)
-		return("Too many arguments on declaration of light");
-	ret = read_pos(buf[1], &(rtx->light->pos), "of light");
-	rtx->light->ratio = ft_atof(buf[2]);
-	if (isnan(rtx->light->ratio))
-		return("Invalid number for ratio of light");
-	if (rtx->light->ratio > 1 || rtx->light->ratio < 0)
-		return ("Value out of range for ratio of light");
-	ret = join(ret, read_color(buf[3], &(rtx->light->color), "of light"));
-	rtx->light->color = lfois(rtx->light->color, rtx->light->ratio);
 	return(ret);
 }
 
